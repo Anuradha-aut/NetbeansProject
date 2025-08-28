9abfc0bc1fce759e382a09d862df003d6d257645
anusingh <anusingh@wz324-690tsw.autuni.aut.ac.nz>
2025-08-28 13:46:12 +1200

    Added Diff

--- PDCDemo_git/src/pdcdemo_git/Diff.java
+++ PDCDemo_git/src/pdcdemo_git/Diff.java
@@ -1,3 +1,23 @@
+<<<<<<< OURS
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pdcdemo_git;
+
+/**
+ *
+ * @author anusingh
+ */
+public class Diff {
+    public static void main(String[] args) {
+        int a= 6;
+        int b= 3;
+        
+        System.out.println("The Diff is: " + (a-b));
+    }
+}
+=======
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -14,3 +34,4 @@ public class Diff {
     }
     
 }
+>>>>>>> THEIRS
