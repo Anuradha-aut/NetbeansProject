28dc96bb39b21dde4438890cf27a27b41ca0e52c
anusingh <anusingh@wz324-690tsw.autuni.aut.ac.nz>
2025-08-28 12:39:28 +1200

    First Commit: added a simple program

---
+++ PDCDemo_git/src/pdcdemo_git/TestGit.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pdcdemo_git;
+
+/**
+ *
+ * @author anusingh
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+        System.out.println(a+b);
+    }
+    
+}
